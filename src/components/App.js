457a82eeec19591bd59e1a3665ce5b4cad8066cd
Thomas Witherow <twitherow@bellsouth.net>
2021-07-26 10:55:17 -0500

    update

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ export const Nutshell = () => (
     <>
       <Route
         render={() => {
-          if (sessionStorage.getItem("App_user")) {
+          if (sessionStorage.getItem("App_user") && ) {
             return (
               <>
                 <NavBar />
