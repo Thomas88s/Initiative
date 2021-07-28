26f69ce0712d171f623de8d62605e48c0e64e2af
Thomas Witherow <twitherow@bellsouth.net>
2021-07-28 15:44:58 -0500

    d

--- src/components/admin/AdminMessageCard.js
+++ src/components/admin/AdminMessageCard.js
@@ -4,6 +4,7 @@ import { MessageContext } from "../messages/MessageProvider"
 import { UsersContext } from "../users/UserProvider"
 import "../messages/Message.css"
 
+
 export const AdminMessageCard = ({ message }) => {
     const { deleteMessage } = useContext(MessageContext)
     const { users } = useContext(UsersContext)
@@ -11,10 +12,11 @@ export const AdminMessageCard = ({ message }) => {
 
     const foundSender = users.find(user => (user.id === message.senderId))
 
+    
     const handleDelete = () => {
         deleteMessage(message.id)
             .then(() => {
-               
+                history.push("/messages")
             })
     }
 
